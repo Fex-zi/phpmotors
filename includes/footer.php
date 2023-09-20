3e663c9858d675bd8d6407179eb9b18de7a507d0
Fex-zi <fexzi87@gmail.com>
2023-09-19 23:56:36 -0600

    test

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<!-- footer.php -->
+<footer >
+    <hr>
+        <p>&copy; 2023 PHP Motors. All time reserved.</p>
+        <p>All images used are believed to be in 'Fair use'. Please notify the author if are not and they will be removed.</p>
+    <?php
+        $current_file_name = basename($_SERVER['PHP_SELF']);
+        $file_last_modified = filemtime($current_file_name); 
+        echo "Last updated: " . date("dS F, Y, h:ia", $file_last_modified)."\n";
+    ?>
+    
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!-- header.php -->
+<header >
+    <div class="container">
+    
+        <div class="logo">
+            <img src="images/site/logo.png" alt="PHP Motors Logo" width="200">
+        </div>
+        <div class="logo2">
+        <a href="#"> My Account</a>
+        </div>
+    </div>
+</header>
