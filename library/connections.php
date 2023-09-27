1b680c977c63048aaafa005130f43d22dd749d6b
Fex-zi <fexzi87@gmail.com>
2023-09-26 21:40:54 -0600

    Enhancement 2

---
+++ library/connections.php
@@ -0,0 +1,22 @@
+<?php 
+
+function phpmotorsConnect(){
+ $server = 'Localhost';
+ $dbname= 'phpmotors';
+ $username = 'Ifeanyi';
+ $password = 'ifesco'; 
+ $dsn = "mysql:host=$server;dbname=$dbname";
+ $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+ // Create the actual connection object and assign it to a variable
+ try {
+  $link = new PDO($dsn, $username, $password, $options);
+  return $link;
+ } catch(PDOException $e) {
+  header('Location: /phpmotors/view/500.php');
+  exit;
+ }
+}
+
+?>
+<?php phpmotorsConnect(); ?>
---
+++ view/500.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta property="og:title" content="phpmotors">
+  <meta property="og:type" content="website">
+  <meta property="og:image" content="https://www.krazybutterfly.com/wp-content/uploads/2023/03/Lagos-Nigeria.jpg">
+  <meta property="og:url" content="https://github.com/Fex-zi/phpmotors">
+  <title>PHP Motors | Template </title>
+  <meta name="description" content="Homepage For PhP Motors">
+  <meta name="author" content="Ifeanyi Ojukwu">
+  <link rel="icon" href="../images/logo.png">
+  <link href="../styles/base.css" rel="stylesheet" media="screen">
+  <link href="../styles/larger.css" rel="stylesheet" media="screen">
+  <link href="../styles/normalize.css" rel="stylesheet" media="screen">
+  
+  
+</head>
+
+<body style='background: url("../images/site/small_check.jpg"); background-size: cover;'>
+<div class="border-bg">
+<?php require_once('../includes/header.php');?>
+
+<?php require_once('../includes/navigation.php'); ?>
+
+<!-- other content here-->
+<main>  
+  
+<div class="div1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Server Error</div>
+<p class="box-p1">Sorry our server seems to experiencing some technical issues. Please check back later.</p>
+
+</main>
+
+<?php require_once('../includes/footer.php'); ?> 
+</div>
+</body>
+</html>
